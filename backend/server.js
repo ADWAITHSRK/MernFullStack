7188e8aeb6e90285ce3e3389e954a8ac561a4bdf
ADWAITHSRK <adwaithkrishna43@gmail.com>
2025-04-13 15:03:35 +0530

    106commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import products from './data/products.js'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './Config/Db.js';
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import orderRoutes from './routes/orderRoutes.js'
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import errorHandler from './middleware/errorMiddleware.js';
 import path from "path";
 import { fileURLToPath } from 'url';
@@ -15,17 +14,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
-const port = 7000;
+const port = process.env.PORT || 7000; 
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-    optionsSuccessStatus: 200
-  };
-  
+  origin: ['http://localhost:5173', 'http://localhost:5174'], 
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
 connectDB();
 
+// Serve static files from the frontend build directory
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(cors(corsOptions));
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
@@ -34,25 +34,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.get('/',(req,res)=>{
-    console.log('Running....')
-})
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-  });
-
-
+// API routes
 app.use('/api/products', productRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/order', orderRoutes);
 
+// Serve the frontend index.html for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+});
 
-
-
+// Error handling middleware
 app.use(errorHandler);
 
-app.listen(port,'0.0.0.0',()=>{
-    console.log(`server is running on port ${port} `)
-})
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
