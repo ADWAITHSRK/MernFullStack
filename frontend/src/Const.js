977513138399ee2a94cbe3e6d369487202693fb3
ADWAITHSRK <adwaithkrishna43@gmail.com>
2025-05-11 19:39:27 +0530

    7-39

--- frontend/src/Const.js
+++ frontend/src/Const.js
@@ -1,5 +1,5 @@
-export const API_URL = "https://mernfullstack-backend-t7mc.onrender.com";
-export const BASE_URL = "https://mernfullstack-backend-t7mc.onrender.com";
+export const API_URL = "https://mern-full-stack-frontend-six.vercel.app";
+export const BASE_URL = "https://mern-full-stack-frontend-six.vercel.app";
 export const PRODUCT_URL = "/api/products";
 export const USER_URL = "/api/user"
 export const ORDER_URL = "/api/order"
